67e46fd9282b7b416c58c3a71b2619b60e4c11fe
thomcgn <kramer.th@outlook.de>
2023-02-03 19:48:23 +0100

    devise und layout updates

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.action_mailer.default_url_options = { host: '167.235.74.206', port: 3000 }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
